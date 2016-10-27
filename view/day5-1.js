1590366ad9ae2f61810852d98822e741a2c2b39c
irna fitriani <irna.fitriani@gmail.com>
2016-10-28 01:16:30 +0700

    Still error in Displaying Result

--- view/day5-1.js
+++ view/day5-1.js
@@ -9,11 +9,33 @@ import{
     Image
 } from 'react-native'
 
+import SearchResults from './day5-2';
+
+function urlForQueryAndPage(key, value, pageNumber) {
+  var data = {
+      country: 'uk',
+      pretty: '1',
+      encoding: 'json',
+      listing_type: 'buy',
+      action: 'search_listings',
+      page: pageNumber
+  };
+  data[key] = value;
+ 
+  var querystring = Object.keys(data)
+    .map(key => key + '=' + encodeURIComponent(data[key]))
+    .join('&');
+ 
+  return 'http://api.nestoria.co.uk/api?' + querystring;
+};
+
 export default class SearchPage extends Component{
     constructor(props){
         super(props);
         this.state = {
-            searchString: 'london'
+            searchString: 'london',
+            isLoading: false,
+            message:''
         }
     }
 
@@ -23,20 +45,61 @@ export default class SearchPage extends Component{
         console.log(this.state.searchString);
     }
 
+    _executeQuery(query) {
+    console.log(query);
+    this.setState({ isLoading: true });
+
+    fetch(query)
+    .then(response => response.json())
+    .then(json => this._handleResponse(json.response))
+    .catch(error =>
+        this.setState({
+        isLoading: false,
+        message: 'Something bad happened ' + error
+      }));
+    }
+
+    _handleResponse(response) {
+        this.setState({ isLoading: false , message: '' });
+        if (response.application_response_code.substr(0, 1) === '1') {
+           this.props.navigator.push({
+                title: 'Results',
+                component: SearchResults,
+                passProps: {listings: response.listings}
+                });
+        } else {
+            this.setState({ message: 'Location not recognized; please try again.'});
+        }
+    }
+ 
+    onSearchPressed() {
+    var query = urlForQueryAndPage('place_name', this.state.searchString, 1);
+    this._executeQuery(query);
+    }
+
     render(){
+         console.log('SearchPage.render');
+         
+        var spinner = this.state.isLoading ?
+        ( <ActivityIndicator
+            size='large'/> ) :
+        ( <View/>);
+        
         return(
             <View style={styles.container}>
                 <View style={styles.flowRight}>
                     <TextInput style={styles.searchInput} placeholder='Search via name or postcode' 
                         value={this.state.searchString}
                         onChange={this.onSearchTextChanged.bind(this)} />
-                    <TouchableHighlight style={styles.button} underlayColor='#99d9f4'>
+                    <TouchableHighlight style={styles.button} underlayColor='#99d9f4'
+                        onPress={this.onSearchPressed.bind(this)}>
                         <Text style={styles.buttonText}>Go</Text>
                     </TouchableHighlight>
                 </View>
                 <TouchableHighlight style={styles.button} underlayColor='#99d9f4'>
                     <Text style={styles.buttonText}>Location</Text>
                 </TouchableHighlight>
+                <Text style={styles.description}>{this.state.message}</Text>
             </View>
         );
     }
